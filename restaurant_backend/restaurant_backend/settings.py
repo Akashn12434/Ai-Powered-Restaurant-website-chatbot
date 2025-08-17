89ed6efe83e96e50fb24b0de82142c333860c793
Akashn12434 <141350500+Akashn12434@users.noreply.github.com>
2025-08-17 12:55:48 +0530

    Update settings.py

--- restaurant_backend/restaurant_backend/settings.py
+++ restaurant_backend/restaurant_backend/settings.py
@@ -150,7 +150,10 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 
-GOOGLE_API_KEY = os.getenv('GOOGLE_API_KEY')
+
+# New Groq key
+GROQ_API_KEY = os.getenv('GROQ_API_KEY')
+
 
 LOGGING = {
     'version': 1,
